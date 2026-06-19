2bddfefaaf6e8d98a842143cbba7126f3fc3f476
NateScarlet <NateScarlet@Gmail.com>
2026-06-19 21:41:46 +0800

    chore: configure unittest

--- scripts/check.ps1
+++ scripts/check.ps1
@@ -6,5 +6,12 @@ if (Test-Path $envFile) {
 & pyright gateway/
 if ($LASTEXITCODE) {
     exit $LASTEXITCODE
-}   
+}
+& python -m unittest discover -s tests -t .
+if ($LASTEXITCODE) {
+    exit $LASTEXITCODE
+}
 & black gateway/
+if ($LASTEXITCODE) {
+    exit $LASTEXITCODE
+}
